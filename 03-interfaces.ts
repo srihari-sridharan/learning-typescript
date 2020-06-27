1521ea70c962b9ee5e1fff53a130e277a9519123
Srihari Sridharan <srihari.sridharan.r@gmail.com>
2020-06-27 21:05:49 +0530

    Update 03-interfaces.ts

--- 03-interfaces.ts
+++ 03-interfaces.ts
@@ -1,5 +1,7 @@
-// TOPIC: 🌟 Interfaces help us define contracts.
+// TOPIC: 🌟 Interfaces
 
+// An interface is a contract.
 
 // IMPORTANT NOTE: ✍🏽 Interfaces do not have an impact on the generated JavaScript code. They are just used by the TypeScript complier for verifying data types to ensure that the code adheres to the interface (contract)!
 
+// TS compiler checks for the structure of objects, i.e, attributes/properties in an object when validating against an interface. Interfaces help us validate duck typing (also known as structural sub-typing. It helps us define contracts within our code and with the code that is outside.
