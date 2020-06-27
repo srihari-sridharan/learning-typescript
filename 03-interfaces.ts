97652f6ae5b6c492952f5c2741baa6228e49a35b
Srihari Sridharan <srihari.sridharan.r@gmail.com>
2020-06-27 19:27:24 +0530

    Getting started with interfaces.

---
+++ 03-interfaces.ts
@@ -0,0 +1,5 @@
+// TOPIC: 🌟 Interfaces help us define contracts.
+
+
+// IMPORTANT NOTE: ✍🏽 Interfaces do not have an impact on the generated JavaScript code. They are just used by the TypeScript complier for verifying data types to ensure that the code adheres to the interface (contract)!
+
